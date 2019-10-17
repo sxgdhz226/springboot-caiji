89f87b5474ae9b1d12796ce63ef71b32aa1e57ce
sxgdhz226 <1600328315@qq.com>
2019-10-17 17:36:38 +0800

    修改springboot欢迎页

---
+++ src/main/java/net/cn/ssd/framework/common/DefultPage.java
@@ -0,0 +1,21 @@
+package net.cn.ssd.framework.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class DefultPage extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName("forward:/static/index.html");
+
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        super.addViewControllers(registry);
+
+    }
+}
